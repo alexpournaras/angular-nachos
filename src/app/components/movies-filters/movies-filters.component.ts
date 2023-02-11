c781c2ba179462a0a83e5feeb0b9db879d482e27
Alexandros Pournaras <apournaras@york.citycollege.eu>
2023-02-11 15:44:14 +0200

    Make filters to have options based on the data of the apis

--- src/app/components/movies-filters/movies-filters.component.ts
+++ src/app/components/movies-filters/movies-filters.component.ts
@@ -1,11 +1,38 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { Movie } from '../../movie';
 
 @Component({
   selector: 'movies-filters',
   templateUrl: './movies-filters.component.html',
   styleUrls: ['./movies-filters.component.css']
 })
-export class MoviesFiltersComponent {
+export class MoviesFiltersComponent implements OnInit {
+  yearsList: number[] = [];
+  genresList: string[] = [];
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.movies$.subscribe((data: { [id: string]: Movie }) => {
+
+      // TODO: Remove this when image becames available
+      delete data['movie002'];
+
+      for (const movie of Object.values(data)) {
+        if (!this.yearsList.includes(movie.releaseYear)) this.yearsList.push(movie.releaseYear);
+      }
+
+      this.yearsList.sort((a: number, b: number) => (b >= a) ? 1 : -1);
+    });
+
+    this.apiService.genres$.subscribe((data) => {
+      for (const genre of Object.keys(data)) {
+        if (!this.genresList.includes(genre)) this.genresList.push(genre);
+      }
+    });
+  }
+
   showGenres: boolean = false;
   showYear: boolean = false;
   showSort: boolean = false;
