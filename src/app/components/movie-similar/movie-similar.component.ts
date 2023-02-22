a9f9b1a1e508f55b07ef70a71742ffd667b35fd1
Alexandros Pournaras <apournaras@york.citycollege.eu>
2023-02-22 22:28:42 +0200

    Implement desktop version of trending 10 movies page

--- src/app/components/movie-similar/movie-similar.component.ts
+++ src/app/components/movie-similar/movie-similar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../api.service';
 import { Movie } from '../../movie';
--- src/app/components/trending10/trending10.component.ts
+++ src/app/components/trending10/trending10.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { Movie } from '../../movie';
 
 @Component({
   selector: 'trending-10',
   templateUrl: './trending10.component.html',
   styleUrls: ['./trending10.component.css']
 })
-export class Trending10Component {
+export class Trending10Component implements OnInit {
+  movies: Movie[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  async ngOnInit() {
+    const movies = await this.apiService.getMovies();
+    movies.sort((a, b) => b.score - a.score);
+    this.movies = movies.slice(0, 10);
+  }
 }
