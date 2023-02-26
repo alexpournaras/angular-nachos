4804a45c5014f350ebce570094084ac543def95b
Alexandros Pournaras <apournaras@york.citycollege.eu>
2023-02-26 16:45:41 +0200

    Fix cookie visibility and functionality

--- src/app/layout/cookie/cookie.component.ts
+++ src/app/layout/cookie/cookie.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cookie',
   templateUrl: './cookie.component.html',
   styleUrls: ['./cookie.component.css']
 })
-export class CookieComponent {
+export class CookieComponent implements OnInit {
+  showCookie: boolean = false;
 
+  constructor() { }
+
+  ngOnInit() {
+    // Check if cookie is accepted
+    if (localStorage.getItem('cookie_accepted') != 'true') {
+      this.showCookie = true;
+    }
+  }
+
+  hideCookie() {
+    this.showCookie = false;
+  }
+
+  acceptCookie() {
+    // Set accepted cookie to local storage
+    localStorage.setItem('cookie_accepted', 'true');
+    this.showCookie = false;
+  }
+  
 }
